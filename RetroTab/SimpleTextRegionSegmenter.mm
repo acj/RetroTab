28f12d23a6f62be4865b82da7d2d10ffc5121cc3
Adam Jensen <acjensen@gmail.com>
2016-08-29 11:31:52 -0400

    Implement +extractTextRegionsFromMat:

--- RetroTab/SimpleTextRegionSegmenter.mm
+++ RetroTab/SimpleTextRegionSegmenter.mm
@@ -4,15 +4,24 @@
 //
 //  Created by Adam Jensen on 8/24/16.
 
+#import "OpenCVUtilities.h"
 #import "SimpleTextRegionSegmenter.h"
 
 @implementation SimpleTextRegionSegmenter
 
 + (NSArray*)extractTextRegionsFromMat:(cv::Mat)mat
 {
-    NSMutableArray* const regions = [NSMutableArray array];
-    // TODO
-    return regions;
+    NSArray* const horizontalHistogram = [OpenCVUtilities computeHorizontalHistogramForMat:mat];
+    NSArray* const verticalHistogram = [OpenCVUtilities computeVerticalHistogramForMat:mat];
+    NSArray* const verticalRanges = [OpenCVUtilities extractContiguousRangesFromHistogram:verticalHistogram withTolerance:5];
+    NSArray* const horizontalRanges = [OpenCVUtilities extractContiguousRangesFromHistogram:horizontalHistogram withTolerance:5];
+    
+//    NSLog(@"Horizontal Histogram: %@", horizontalHistogram);
+//    NSLog(@"Vertical Histogram: %@", verticalHistogram);
+//    NSLog(@"Vertical Ranges: %@", verticalRanges);
+//    NSLog(@"Horizontal Ranges: %@", horizontalRanges);
+    
+    return [OpenCVUtilities extractTextBoundingRectsForCanvasWithSize:CGSizeMake(mat.cols, mat.rows) horizontalRanges:horizontalRanges verticalRanges:verticalRanges];
 }
 
 @end
