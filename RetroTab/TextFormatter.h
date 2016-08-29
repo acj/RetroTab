094bb944a9a3a6f9af7722e8f523c7a2e9b46cf9
Adam Jensen <acjensen@gmail.com>
2016-08-29 11:47:53 -0400

    Add TextFormatter protocol

---
+++ RetroTab/TextFormatter.h
@@ -0,0 +1,13 @@
+//
+//  TextFormatter.h
+//  RetroTab
+//
+//  Created by Adam Jensen on 8/29/16.
+//  Copyright © 2016 Adam Jensen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TextFormatter
++ formatText:(NSArray*)textRowArray;
+@end
